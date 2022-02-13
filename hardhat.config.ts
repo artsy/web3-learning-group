5f9a50b571a1e1f4ee78b29062739a275d887df7
Pavlos Vinieratos <pvinis@gmail.com>
2022-02-13 21:23:11 +0000

    working

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -19,14 +19,11 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
     }
 })
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
 const config: HardhatUserConfig = {
     solidity: "0.8.11",
     networks: {
         ropsten: {
-            url: process.env.ROPSTEN_URL || "",
+            url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}/`,
             accounts:
                 process.env.PRIVATE_KEY !== undefined
                     ? [process.env.PRIVATE_KEY]
@@ -41,5 +38,4 @@ const config: HardhatUserConfig = {
         apiKey: process.env.ETHERSCAN_API_KEY,
     },
 }
-
 export default config
